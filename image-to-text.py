8f049f8ece208277e40349d290809a07831daa6f
Mukul Kumar Singh Chauhan <mukul.mschauhan@gmail.com>
2024-09-06 10:23:37 +0530

    Update image-to-text.py

--- image-to-text.py
+++ image-to-text.py
@@ -35,7 +35,7 @@ if uploaded_file is not None:
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
 
-submit=st.button("Do the Magic❗")
+submit=st.button("✦•┈๑⋅⋯Do the Magic❗⋯⋅๑┈•✦")
 
 ## If ask button is clicked
 
